0f6f500e68582f119f4ac834aeb8d8601cb52bb7
huyngse <ngogiahuy775309@gmail.com>
2025-09-21 15:38:49 +0700

    Add world map data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,37 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, GeoJSON } from "react-leaflet";
+import worldData from "@/data/custom.geo.json";
+import type { Feature, FeatureCollection, Geometry } from "geojson";
+import { stringToColor } from "@/lib/color";
+
+const typedWorldData = worldData as FeatureCollection;
+
 
 const HomePage = () => {
+  const style = (feature?: Feature<Geometry, any>) => {
+    const name = feature?.properties?.name ?? "unknown";
+    return {
+      fillColor: stringToColor(name),
+      weight: 1,
+      color: "white",
+      fillOpacity: 0.7,
+    };
+  };
+
   return (
-    <main>
+    <main className="caret-amber-200">
       <MapContainer
         center={[35.6895, 139.6917]}
-        zoom={13}
-        style={{ height: "100vh", width: "100%" }}
+        zoom={3}
+        style={{ height: "100vh", width: "100%", backgroundColor: "white" }}
+        minZoom={2}
+        maxZoom={5}
+        maxBounds={[
+          [-90, -180],
+          [90, 180],
+        ]}
+        maxBoundsViscosity={1.0}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <GeoJSON data={typedWorldData} style={style} />
         <Marker position={[35.6895, 139.6917]}>
           <Popup>こんにちは！ This is Tokyo! 🗼</Popup>
         </Marker>
